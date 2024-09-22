a593dde3dd69d7486dd01c166602f2523112f9c1
Alexander Nikiforov <nikiforov.java@gmail.com>
2024-09-23 00:08:44 +0400

    Добавлять новую задачу через html форму

--- src/main/java/com/alexnikiforov/todoapp/todoapp/controllers/TodoController.java
+++ src/main/java/com/alexnikiforov/todoapp/todoapp/controllers/TodoController.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,7 +24,15 @@ public class TodoController implements CommandLineRunner {
     public String index(Model model) {
         List<TodoItem> allTodos = todoItemRepository.findAll();
         model.addAttribute("todoItems", allTodos);
+        model.addAttribute("newTodo", new TodoItem());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute TodoItem todoItem) {
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
     }
 
     @Override
